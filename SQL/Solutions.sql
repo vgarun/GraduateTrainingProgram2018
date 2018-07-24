ec4b4c2d990503a3a0ad69192441c71137fd80bd
vgarun <arun.govindaraju@gmail.com>
2018-07-24 14:50:40 +0530

    Create Solutions.sql

---
+++ SQL/Solutions.sql
@@ -0,0 +1,8 @@
+-- ProblemSet<No.>, November <Date> <Year>
+-- Submission by <Your Accenture Email.id> 
+
+/* <Question from the ProblemSet> */
+
+< Your SQL Goes Here >
+
+/* Your Record Count from the SQL execution */
